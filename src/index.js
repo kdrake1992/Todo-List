f765be07335ae71d24570656ceeb25da97555c08
Kevin Drake <kdrake1992@outlook.com>
2022-06-17 23:45:39 +0900

    Created factory function for Todo items

--- src/index.js
+++ src/index.js
@@ -1,4 +1,40 @@
 // Imports CSS
 import './style.css'
 
-console.log("test");
+// Factory Function Todo Item
+const itemToDo = (title, details, dueDate, priority) => {
+    title = title;
+    details = details;
+    dueDate = dueDate;
+    priority = priority;
+    let note = "";
+    let checklist = "no";
+
+    const setTitle = (newTitle) => {
+        obj.title = newTitle;
+    };
+    const setDetails = (newDetails) => {
+        obj.details = newDetails;
+    };
+    const setDueDate = (newDueDate) => {
+        obj.dueDate = newDueDate;
+    };
+    const setPriority = (newPriority) => {
+        obj.priority = newPriority;
+    };
+    const setNote = (newNote) => {
+        obj.note = newNote;
+    };
+    const setCheck = (check) => {
+        if(check === "yes") {
+            obj.checklist = "yes"
+        }
+        else {
+            obj.checklist = "no"
+        };
+    };
+    const obj = {title, details, dueDate, priority, note, checklist, setTitle, setDetails,
+        setDueDate, setPriority, setNote, setCheck};
+
+    return obj;
+}

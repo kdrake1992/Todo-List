95010e073efd188ec6787b384f092451d4504614
Kevin Drake <kdrake1992@outlook.com>
2022-06-20 23:38:47 +0900

    Added add task function, needs more work

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 // Imports CSS
 import './style.css'
 
-// Import date-fns webpack
-import { compareAsc, format } from 'date-fns';
-// const today = new Date(2022, 6, 18); example
-
 // Fontawesome Free
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
+
+// Import date-fns webpack
+import { compareAsc, format, startOfDay } from 'date-fns';
 
 // Factory Function Todo Item
 const itemToDo = (title, details, dueDate, priority) => {
@@ -49,9 +48,9 @@ const itemToDo = (title, details, dueDate, priority) => {
     return obj;
 }
 
-const first = itemToDo("Clean Room", "Clean room, its been a few days.", new Date(2022, 5, 21), "low");
-const third = itemToDo("Cut Grass", "Grass is getting tall.", new Date(2022, 5, 20), "medium");
-const fifth = itemToDo("Study Japanese", "JLPT test soon.", new Date(2022, 6, 4), "high");
+let first = itemToDo("Clean Room", "Clean room, its been a few days.", new Date(2022, 5, 21), "low");
+let third = itemToDo("Cut Grass", "Grass is getting tall.", new Date(2022, 5, 20), "medium");
+let fifth = itemToDo("Study Japanese", "JLPT test soon.", new Date(2022, 6, 4), "high");
 
 // Create projects or lists of of todo items.
 const project = () => {
@@ -73,8 +72,9 @@ const project = () => {
 
 // Create default projects; all, this week, and important
 const allProjects = project();
-const thisWeek = project();
-const highPriority = project();
+const todaysTasks = project()
+const thisWeeksTasks = project();
+const importantTasks = project();
 
 
 // Filler data
@@ -112,24 +112,20 @@ const start = function() {
 const top = function() {
     topMenu.innerHTML = 
     `
-    <i class="fa-solid fa-bars"></i>
 
-    <div class="username">
-        <div class="userintro">
-            <p id="small">Hi there, Kevin Drake!</p>
-        </div>
+    <div class="logo">
+        <h1><i class="fa-solid fa-fire"></i></h1>
+        <h1>Yarukoto</h1>
     </div>
 
-    <div class="searchBar">
-        <form action="#">
-            <div class = "input-wrapper">
-                <i class="fa-solid fa-magnifying-glass"></i>
-                <input type="text" name="search" id="search" placeholder="      Search:">
-            </div>
-        </form>
+    <div class="userName">
+        <p id="small">Hello there, Kevin Drake!</p>
     </div>
 
-    <button>New</button>
+    <div class="today">
+        <div>${todaysDate()}</div>
+        <div id ="clock" onload="currentTime()"></div>
+    </div>
     `;
 
     return topMenu;
@@ -138,21 +134,19 @@ const top = function() {
 const side = function() {
     sideMenu.innerHTML = 
     `
-    <div class="logo">
-        <h1><i class="fa-solid fa-fire"></i></h1>
-        <h1>Yarukoto</h1>
-    </div>
     <ol class="toDoList">
         <li><h2><u>Menu</u></h2></li>
-        <li><i class="fa-solid fa-inbox"></i> All Tasks</li>
-        <li><i class="fa-solid fa-calendar-day"></i> Today</li>
-        <li><i class="fa-solid fa-calendar-week"></i> This Week</li>
-        <li><i class="fa-solid fa-bolt-lightning"></i> Important</li>
+        <li id="allTasks"><i class="fa-solid fa-inbox"></i> All Tasks</li>
+        <li id="today"><i class="fa-solid fa-calendar-day"></i> Today</li>
+        <li id="currentWeek"><i class="fa-solid fa-calendar-week"></i> This Week</li>
+        <li id="important"><i class="fa-solid fa-bolt-lightning"></i> Important</li>
     </ol>
     <ol class="toDoList">
         <li><h2><u>Projects</u></h2></li>
-        <li><i class="fa-solid fa-plus"></i> New Project</li>
+        <li id="addProject"><i class="fa-solid fa-plus"></i> New Project</li>
     </ol>
+
+    <div id="addTask"> <i class="fa-solid fa-circle-plus fa-4x"></i></div>
     `;
     
     return sideMenu;
@@ -176,6 +170,138 @@ const foot = function() {
     return footer;
 }
 
+// Today's date
+const todaysDate = function() {
+    let today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const yyyy = today.getFullYear();
+    
+    today = mm + '/' + dd + '/' + yyyy;
+    return today;
+}
+
+// Initial site load
 start().forEach(e=> {
     body.appendChild(e);
 })
+
+// Current time
+function currentTime() {
+    let date = new Date(); 
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    let session = "AM";
+  
+    if(hh === 0){
+        hh = 12;
+    }
+    if(hh > 12){
+        hh = hh - 12;
+        session = "PM";
+     }
+  
+     hh = (hh < 10) ? "0" + hh : hh;
+     mm = (mm < 10) ? "0" + mm : mm;
+     ss = (ss < 10) ? "0" + ss : ss;
+      
+     let time = hh + ":" + mm + ":" + ss + " " + session;
+  
+    document.getElementById("clock").innerText = time; 
+    let t = setTimeout(function(){ currentTime() }, 1000);
+  };
+  currentTime();
+
+// Create DOM elements
+const tasks = document.getElementById("allTasks");
+const day = document.getElementById("today");
+const week = document.getElementById("currentWeek");
+const thunder = document.getElementById("important");
+
+const plusProject = document.getElementById("addProject");
+const plusTasks = document.getElementById("addTask");
+
+tasks.addEventListener("click", e=> {
+    console.log("1")
+})
+
+day.addEventListener("click", e=> {
+    console.log("2")
+})
+
+week.addEventListener("click", e=> {
+    console.log("3")
+})
+
+thunder.addEventListener("click", e=> {
+    console.log("4")
+})
+
+plusProject.addEventListener("click", e=> {
+    console.log("5")
+})
+
+plusTasks.addEventListener("click", e=> {
+    const taskForm = document.createElement("div");
+    taskForm.classList.add("formTask");
+
+    taskForm.innerHTML = `
+        <form id="addFormTask">
+        <legend>New Task</legend>
+
+        <div class="formInput">
+            <label for="task">Task:</label>
+            <input type="text" name="task" id="task" placeholder="Task">
+        </div>
+
+        <div class="formInput">
+            <label for="details">Details:</label>
+            <textarea name="details" id="details" cols="30" rows="10"
+            placeholder="Task Details(Optional)"></textarea>
+        </div>
+
+        <div class="dateAndPrio">
+            <div class="formInput">
+                <label for="date">Due Date:</label>
+                <input type="date" name="date" id="date">
+            </div>
+
+            <div class="formInput">
+                <label for="priority">Priority:</label>
+                <select name="priority" id="priority">
+                    <option value="Low">Low</option>
+                    <option value="Medium">Medium</option>
+                    <option value="High">High</option>
+                </select>
+            </div>
+        </div>
+
+        <div class="submitAndReset">
+            <button type="submit" name="button" id="submit">Submit</button>
+            <button type="reset">Clear</button>
+        </div>
+        </form>
+        `
+        document.body.appendChild(taskForm);
+
+        topMenu.classList.add(".fade");
+        sideMenu.classList.add(".fade");
+        main.classList.add(".fade");
+        footer.classList.add(".fade");
+
+        document.getElementById("submit").addEventListener('click', e=> {
+            e.preventDefault();
+
+            let task = document.getElementById("task");
+            let descrip = document.getElementById("details");
+            let date = document.getElementById("date");
+            let prio = document.getElementById("priority");
+
+            let newTask = itemToDo(task.value, descrip.value, date.value, prio.value);
+            allProjects.addItem(newTask);
+            console.log(allProjects.toDoItems);
+        })
+})
+
+console.log(allProjects.toDoItems);
